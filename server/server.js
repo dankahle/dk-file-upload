c208297420f44a915a986f668bde141e6ca6f0c7
dan kahle <dan.kahle@gmail.com>
2018-04-28 06:36:54 -0700

    latest

--- server/server.js
+++ server/server.js
@@ -7,6 +7,17 @@ const express = require('express'),
   morgan = require('morgan'),
   router = require('router');
 
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+});
+
+// config data sources
+mongoose.connect(config.mongoUri)
+  .then(() => console.log(`mongoose connected on: ${config.mongoUri}`),
+    err => console.log(`mongoose connection error: ${config.mongoUri}`, err));
+
+
 
 const app = require('express');
 app.use('/api/file', fileRouter);
